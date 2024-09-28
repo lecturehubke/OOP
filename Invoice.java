c72430a2049f9f7068435684c8eb2dcd647f20e9
lecturehubke <michael.kagiri@dkut.ac.ke>
2024-09-28 13:30:23 +0300

    Add files via upload

---
+++ Invoice.java
@@ -0,0 +1,35 @@
+
+public class Invoice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+System.out.println("*************************Adamson Computers Ltd***************************");
+System.out.println("#99 ACS Building Gilmore Ave. New Mla.. Quezon City  *  Tel. No:123-4567");	
+System.out.println("\t\t VAT REG. TIN 098-765-4321-0000");
+System.out.println("\t\t\t SALES INVOICE\n\n");
+System.out.println("Sold to:Jim Kim\t\t\t\t\t\tDate:28/04/2020");
+System.out.println("Address:private bag\t\t\t\t\tContact Number:0100100100\n\n");
+System.out.println("==============================================================================");
+System.out.println("Qty\t|\t Item Description\t|\t Unit Price\t|Amount");
+System.out.println("==============================================================================");
+System.out.println( "4\t|\t Pencil\t\t\t|\t 20\t\t|80.00");
+System.out.println("------------------------------------------------------------------------------");
+System.out.println( "10\t|\t Duster\t\t\t|\t 50\t\t|500.00");
+System.out.println("------------------------------------------------------------------------------");
+System.out.println( "9\t|\t Pens\t\t\t|\t 30\t\t|270.00");
+System.out.println("------------------------------------------------------------------------------");
+System.out.println( "10\t|\t crayon\t\t\t|\t 80\t\t|800.00");
+System.out.println("==============================================================================");
+System.out.println("\t\t\t\t\t\t|Sub Total\t|1650.00");
+System.out.println("\t\t\t\t\t\t==============================");
+System.out.println("\t\t\t\t\t\t|VAT(16%)\t|264.00");
+System.out.println("\t\t\t\t\t\t==============================");
+System.out.println("\t\t\t\t\t\t|Total Amount\t|1914.00");
+System.out.println("\t\t\t\t\t\t==============================");
+System.out.println("");
+
+
+	}
+
+}
